d11038a24f4d50438fc5d145a61a9a4d4631b9dd
GAJA-hash <gaja5588@gmail.com>
2021-08-23 20:59:28 +0530

    Hello world program in eclipse using maven

---
+++ helloworld_gaja/src/test/java/helloworld_gaja/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package helloworld_gaja;
+
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+	
+	@Test
+	public void testHelloWorld() { //Function
+
+		System.out.println("..Hello World...!");
+		
+	}
+		
+}
